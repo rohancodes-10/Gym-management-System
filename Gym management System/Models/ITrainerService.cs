2d2f88814cb3367acad1c1120128f5e86abe9f05
rohancodes-10 <rohankc9988@gmail.com>
2026-04-23 21:17:25 +0545

    Adding ItrainerService interface

---
+++ Gym management System/Models/ITrainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace Gym_management_System.Models
+{
+    public interface ITrainerService
+    {
+         IEnumerable<Trainer> GetTrainers();
+         Trainer? GetTrainer(int id);
+         Trainer AddTrainer(Trainer trainer);
+        Trainer Update(Trainer Changes);
+    }
+}
