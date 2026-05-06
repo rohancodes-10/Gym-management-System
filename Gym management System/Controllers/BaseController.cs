4756e87f5220c401a01a22acdce90b31a005d078
rohancodes-10 <rohankc9988@gmail.com>
2026-05-06 20:35:33 +0545

    Editing base controller

--- Gym management System/Controllers/BaseController.cs
+++ Gym management System/Controllers/BaseController.cs
@@ -4,6 +4,19 @@ namespace Gym_management_System.Controllers
 {
     public class BaseController : Controller
     {
-      
+        protected string? GetRole() =>
+              HttpContext.Session.GetString("userRole");
+        protected int? GetUserId() =>
+            HttpContext.Session.GetInt32("userId");
+        protected int? GetGymId() =>
+            HttpContext.Session.GetInt32("GymId");
+        protected string? GetUserName() =>
+            HttpContext.Session.GetString("userName");
+        protected bool IsloggedIn() => GetRole() != null;
+        protected bool IsOwner() => GetRole() == "Owner";
+        protected bool IsManager() => GetRole() == "Manager";
+        protected bool IsTrainer() => GetRole() == "Trainer";
+        protected bool IsMember() => GetRole() == "Member";
+
     }
 }
