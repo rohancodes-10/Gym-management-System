b5e7f11ab91ca3b4fa1cad001aa43155afd6bf14
rohancodes-10 <rohankc9988@gmail.com>
2026-05-13 12:38:52 +0545

    Adding Membershipplan and membershippayment class

---
+++ Gym management System/Models/MembershipPayment/MembershipPayment.cs
@@ -0,0 +1,26 @@
+﻿using Gym_management_System.Models.Members;
+using Gym_management_System.Models.MembershipPlan;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+namespace Gym_management_System.Models.MembershipPayment
+{
+    public class MembershipPayment
+    {
+        public int id {  get; set; }
+        
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+
+        public int MembershipPlanId {  get; set; }
+        public MembershipPlan MembershipPlan { get; set; }
+        public int AmountPaid { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; } = "Active"; // Active / Expired
+
+
+    }
+}
---
+++ Gym management System/Models/MembershipPlan/MembershipPlan.cs
@@ -0,0 +1,13 @@
+﻿using Gym_management_System.Models.Gyms;
+namespace Gym_management_System.Models.MembershipPlan
+{
+    public class MembershipPlan
+    {
+        public int id { get; set; }
+        public string MembershipPlanName { get; set; }=string.Empty;
+        public int GymId { get; set; }
+        public Gym Gym { get; set; }
+        public int DurationInDays { get; set; }
+        public Decimal price { get; set; }
+    }
+}
