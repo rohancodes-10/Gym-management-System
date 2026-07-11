051c8adfadfb001a98e81aa918d14787e28a8856
rohancodes-10 <rohankc9988@gmail.com>
2026-07-11 22:08:52 +0545

    filling controller with required method

--- Gym management System/Controllers/AttendenceController.cs
+++ Gym management System/Controllers/AttendenceController.cs
@@ -1,9 +1,35 @@
-﻿using Gym_management_System.Models.Members;
+﻿using Gym_management_System.Models.Attendences;
+using Gym_management_System.Models.Members;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gym_management_System.Controllers
 {
-    public class AttendenceController
+    public class AttendenceController:BaseController
     {
-        
+        private readonly IAttendence attendenceService;
+        public AttendenceController(IAttendence _attendenceService) 
+        {
+            attendenceService= _attendenceService;
+        }
+        public IActionResult index(int gymId)
+        {
+            ViewData["CurrendGymId"] = gymId;
+            var model=attendenceService.GetTodaysAttendenceStatus(gymId);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult MarkPresent(int memberId,int gymId)
+        {
+            attendenceService.MarkPresent(memberId);
+            return RedirectToAction("index", new { gymId });
+
+        }
+        [HttpPost]
+        public IActionResult MarkCheckOut(int memberId, int gymId)
+        {
+            attendenceService.MarkCheckOut(memberId);
+            return RedirectToAction("index", new { gymId });
+
+        }
     }
 }
