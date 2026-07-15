8fee9106584a1022a9fbb62f83e027579d5033c1
rohancodes-10 <rohankc9988@gmail.com>
2026-07-15 21:00:34 +0545

    viewmodels for calender

---
+++ Gym management System/ViewModels/AttendenceViewModels/CalenderDayViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym_management_System.ViewModels.AttendenceViewModels
+{
+    public class CalenderDayViewModel
+    {
+        public DateOnly? Date { get; set; }
+        public bool IsPresent { get; set; }
+        public bool IsFuture { get; set; }
+        public bool IsToday { get; set; }
+     public TimeOnly? CheckIn { get; set; }
+        public TimeOnly? CheckOut { get; set; }
+    }
+}
---
+++ Gym management System/ViewModels/AttendenceViewModels/MemberAttendenceCalenderViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym_management_System.ViewModels.AttendenceViewModels
+{
+    public class MemberAttendenceCalenderViewModel
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string MonthName => new DateTime(Year, Month, 1).ToString("MMMM yyyy");
+        public List<CalenderDayViewModel> Days { get; set; } = new();
+        public int TotalPresentDays { get; set; }
+        public int TotalDaysElapsed { get; set; }
+    }
+}
